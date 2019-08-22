470c4c0aca22b8f1c71a9b469f7e69b961969fdc
Sebastian Landwehr <info@dword-design.de>
2019-08-22 02:05:13 +0200

    prepare for publish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import glob from 'glob-promise'
 import userHome from 'user-home'
 import { readJson, realpath, lstat } from 'fs-extra'
-import { map, filter } from '@functions'
-import { unary } from 'lodash/fp'
-import { resolve } from 'path'
+import { map, filter, invoke, unary, promiseAll } from '@functions'
+import { resolve as _resolve } from 'path'
 
-const promiseAll = promises => Promise.all(promises)
+const resolve = (...paths) => path => _resolve(path, ...paths)
+
+const filterAsync = callback => async collection => collection
+  |> map(callback) |> promiseAll |> await
+  |> (filterResults => collection |> filter((element, index) => filterResults[index]))
 
 export default async () => glob(`${userHome}/.config/yarn/link/**`) |> await
-  |> (async packagePaths => packagePaths
-    |> map(lstat |> unary) |> promiseAll |> await
-    |> (stats => packagePaths |> filter((packagePath, index) => stats[index].isSymbolicLink()))
-  ) |> await
+  |> filterAsync(async packagePath => packagePath |> lstat |> await |> invoke('isSymbolicLink')) |> await
   |> map(realpath |> unary) |> promiseAll |> await
-  |> map(resolvedPath => readJson(resolve(resolvedPath, 'package.json'))) |> promiseAll |> await
+  |> map(resolve('package.json'))
+  |> map(readJson |> unary) |> promiseAll |> await
   |> map('name')
