1db5411ce4749e5919562a808b46ca6841b4b8b6
Sebastian Landwehr <info@dword-design.de>
2019-08-22 09:07:58 +0200

    use filterAsync from functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import glob from 'glob-promise'
 import userHome from 'user-home'
 import { readJson, realpath, lstat } from 'fs-extra'
-import { map, filter, invoke, unary, promiseAll } from '@functions'
+import { map, invoke, unary, promiseAll, filterAsync } from '@functions'
 import { resolve as _resolve } from 'path'
 
 const resolve = (...paths) => path => _resolve(path, ...paths)
-
-const filterAsync = callback => async collection => collection
-  |> map(callback) |> promiseAll |> await
-  |> (filterResults => collection |> filter((element, index) => filterResults[index]))
 
 export default async () => glob(`${userHome}/.config/yarn/link/**`) |> await
   |> filterAsync(async packagePath => packagePath |> lstat |> await |> invoke('isSymbolicLink')) |> await
