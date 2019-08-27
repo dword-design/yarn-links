eaaf0e8b3c557b72bd94e3744652cc5102f61226
Sebastian Landwehr <info@dword-design.de>
2019-08-27 17:00:06 +0200

    use global-dirs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import glob from 'glob-promise'
-import userHome from 'user-home'
 import { readJson, realpath, lstat } from 'fs-extra'
 import { map, invoke, unary, promiseAll, filterAsync } from '@functions'
 import { resolve as _resolve } from 'path'
+import { yarn as yarnDirs } from 'global-dirs'
 
 const resolve = (...paths) => path => _resolve(path, ...paths)
 
-export default async () => glob(`${userHome}/.config/yarn/link/**`) |> await
+export default async () => glob(`${yarnDirs.prefix}/link/**`) |> await
   |> filterAsync(async packagePath => packagePath |> lstat |> await |> invoke('isSymbolicLink')) |> await
   |> map(realpath |> unary) |> promiseAll |> await
   |> map(resolve('package.json'))
