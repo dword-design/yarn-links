d4bba044996821e36a63b6bee99d51cc7dc7ce5b
Sebastian Landwehr <landwehr@zebralog.de>
2019-09-04 15:19:52 +0200

    replace buggy library

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import glob from 'glob-promise'
 import { readJson, realpath, lstat } from 'fs-extra'
 import { map, invoke, unary, promiseAll, filterAsync } from '@functions'
 import { resolve as _resolve } from 'path'
-import { yarn as yarnDirs } from 'global-dirs'
+import getYarnPrefix from 'yarn-config-directory'
 
 const resolve = (...paths) => path => _resolve(path, ...paths)
 
-export default async () => glob(`${yarnDirs.prefix}/link/**`) |> await
+export default async () => glob(`${getYarnPrefix()}/**`) |> await
   |> filterAsync(async packagePath => packagePath |> lstat |> await |> invoke('isSymbolicLink')) |> await
   |> map(realpath |> unary) |> promiseAll |> await
   |> map(resolve('package.json'))
