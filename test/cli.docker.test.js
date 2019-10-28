485e852fbcbc113218c3dedd6b9c2bd9766a636e
dword <sebastianlandw@gmail.com>
2019-10-28 21:50:14 +0000

    use base

--- test/cli.docker.test.js
+++ test/cli.docker.test.js
@@ -4,27 +4,27 @@ import { spawn } from 'child-process-promise'
 import { removeFile } from 'fs-extra'
 import outputFiles from 'output-files'
 
-test('empty', async () => {
+it('empty', async () => {
   const { stdout } = await spawn('yarn-links', { capture: ['stdout'] })
   expect(stdout).toEqual('')
 })
 
-test('two links', async () => {
+it('two links', async () => {
   await outputFiles(userHome, {
     'package-a/package.json': JSON.stringify({ name: 'package-a' }),
     'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
   })
-  await spawn('yarn', 'link', { cwd: `${userHome}/package-a` })
-  await spawn('yarn', 'link', { cwd: `${userHome}/package-b` })
-  const { stdout } = await spawn('yarn-links', { capture: ['stdout'] })
+  await spawn('yarn', ['link'], { cwd: `${userHome}/package-a` })
+  await spawn('yarn', ['link'], { cwd: `${userHome}/package-b` })
+  const { stdout } = await spawn('yarn-links', [], { capture: ['stdout'] })
   expect(stdout).toEqual('@vendor/package-b\npackage-a\n')
   await Promise.all([
     (async () => {
-      await spawn('yarn', 'unlink', { cwd: P.resolve(userHome, 'package-a') })
+      await spawn('yarn', ['unlink'], { cwd: P.resolve(userHome, 'package-a') })
       await removeFile(P.resolve(userHome, 'package-a'))
     })(),
     (async () => {
-      await spawn('yarn', 'unlink', { cwd: P.resolve(userHome, 'package-b') })
+      await spawn('yarn', ['unlink'], { cwd: P.resolve(userHome, 'package-b') })
       await removeFile(P.resolve(userHome, 'package-b'))
     })(),
   ])
--- test/index.docker.test.js
+++ test/index.docker.test.js
@@ -5,23 +5,23 @@ import outputFiles from 'output-files'
 import { spawn } from 'child-process-promise'
 import { removeFile } from 'fs-extra'
 
-test('empty', async () => expect(await yarnLinks()).toEqual([]))
+it('empty', async () => expect(await yarnLinks()).toEqual([]))
 
-test('two links', async () => {
+it('two links', async () => {
   await outputFiles(userHome, {
     'package-a/package.json': JSON.stringify({ name: 'package-a' }),
     'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
   })
-  await spawn('yarn', 'link', { cwd: `${userHome}/package-a` })
-  await spawn('yarn', 'link', { cwd: `${userHome}/package-b` })
+  await spawn('yarn', ['link'], { cwd: `${userHome}/package-a` })
+  await spawn('yarn', ['link'], { cwd: `${userHome}/package-b` })
   expect(await yarnLinks()).toEqual('@vendor/package-b\npackage-a\n')
   await Promise.all([
     (async () => {
-      await spawn('yarn', 'unlink', { cwd: P.resolve(userHome, 'package-a') })
+      await spawn('yarn', ['unlink'], { cwd: P.resolve(userHome, 'package-a') })
       await removeFile(P.resolve(userHome, 'package-a'))
     })(),
     (async () => {
-      await spawn('yarn', 'unlink', { cwd: P.resolve(userHome, 'package-b') })
+      await spawn('yarn', ['unlink'], { cwd: P.resolve(userHome, 'package-b') })
       await removeFile(P.resolve(userHome, 'package-b'))
     })(),
   ])
