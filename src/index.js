9a986ea41eb1719f3c40fad1974fd5a85f802e22
dword <sebastianlandw@gmail.com>
2020-07-04 11:30:10 +0000

    fix: upgrades

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import glob from 'glob-promise'
-import { readJson, realpath, lstat } from 'fs-extra'
-import { map, unary, promiseAll, filterAsync } from '@dword-design/functions'
+import {
+  filterAsync,
+  identity,
+  map,
+  promiseAll,
+  sortBy,
+  unary,
+} from '@dword-design/functions'
+import { lstat, readJson, realpath } from 'fs-extra'
+import globby from 'globby'
 import P from 'path'
 import getYarnPrefix from 'yarn-config-directory'
 
 export default async () => {
-  const candidates = await glob(`${getYarnPrefix()}/**`)
-  const symlinks = candidates |> filterAsync(async candidate => (lstat(candidate) |> await).isSymbolicLink()) |> await
+  const candidates =
+    globby(`${getYarnPrefix()}/**`, {
+      onlyDirectories: true,
+    })
+    |> await
+    |> sortBy(identity)
+  const symlinks =
+    candidates
+    |> filterAsync(async candidate =>
+      (lstat(candidate) |> await).isSymbolicLink()
+    )
+    |> await
   const packagePaths = symlinks |> map(unary(realpath)) |> promiseAll |> await
-  const packageJsons = packagePaths |> map(path => P.resolve(path, 'package.json'))
+  const packageJsons =
+    packagePaths |> map(path => P.resolve(path, 'package.json'))
   const packages = packageJsons |> map(unary(readJson)) |> promiseAll |> await
   return packages |> map('name')
 }
--- test/cli/empty.test.js
+++
@@ -1,6 +0,0 @@
-import { spawn } from 'child-process-promise'
-
-export default async () => {
-  const { stdout } = await spawn('yarn-links', [], { capture: ['stdout'] })
-  expect(stdout).toEqual('')
-}
