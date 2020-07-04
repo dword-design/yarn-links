6788c7d83b7d9d9146ba6a88374dbceefa5cb1f8
dword <sebastianlandw@gmail.com>
2020-07-04 12:29:11 +0000

    fix: fix tests

--- src/cli.spec.js
+++ src/cli.spec.js
@@ -1,39 +1,39 @@
 import execa from 'execa'
 import { remove } from 'fs-extra'
 import outputFiles from 'output-files'
-import P from 'path'
-import userHome from 'user-home'
+import withLocalTmpDir from 'with-local-tmp-dir'
 
 export default {
   empty: async () => {
     const output = await execa(require.resolve('./cli'), { all: true })
     expect(output.stdout).toEqual('')
   },
-  valid: async () => {
-    await outputFiles(userHome, {
-      'package-a/package.json': JSON.stringify({ name: 'package-a' }),
-      'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
-    })
-    await execa.command('yarn link', { cwd: `${userHome}/package-a` })
-    await execa.command('yarn link', { cwd: `${userHome}/package-b` })
-    try {
-      const output = await execa(require.resolve('./cli'), { all: true })
-      expect(output.all).toEqual('  - @vendor/package-b\n  - package-a')
-    } finally {
-      await Promise.all([
-        (async () => {
-          await execa.command('yarn unlink', {
-            cwd: P.resolve(userHome, 'package-a'),
-          })
-          await remove(P.resolve(userHome, 'package-a'))
-        })(),
-        (async () => {
-          await execa.command('yarn unlink', {
-            cwd: P.resolve(userHome, 'package-b'),
-          })
-          await remove(P.resolve(userHome, 'package-b'))
-        })(),
-      ])
-    }
-  },
+  valid: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        'package-a/package.json': JSON.stringify({ name: 'package-a' }),
+        'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
+      })
+      await execa.command('yarn link', { cwd: 'package-a' })
+      await execa.command('yarn link', { cwd: 'package-b' })
+      try {
+        const output = await execa(require.resolve('./cli'), { all: true })
+        expect(output.all).toEqual('  - @vendor/package-b\n  - package-a')
+      } finally {
+        await Promise.all([
+          (async () => {
+            await execa.command('yarn unlink', {
+              cwd: 'package-a',
+            })
+            await remove('package-a')
+          })(),
+          (async () => {
+            await execa.command('yarn unlink', {
+              cwd: 'package-b',
+            })
+            await remove('package-b')
+          })(),
+        ])
+      }
+    }),
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,31 +1,28 @@
 import execa from 'execa'
 import { remove } from 'fs-extra'
 import outputFiles from 'output-files'
-import P from 'path'
-import userHome from 'user-home'
+import withLocaltmpDir from 'with-local-tmp-dir'
 
 import self from '.'
 
 export default {
   empty: async () => expect(await self()).toEqual([]),
-  'two links': async () => {
-    await outputFiles(userHome, {
-      'package-a/package.json': JSON.stringify({ name: 'package-a' }),
-      'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
-    })
-    await execa.command('yarn link', { cwd: `${userHome}/package-a` })
-    await execa.command('yarn link', { cwd: `${userHome}/package-b` })
-    try {
-      expect(await self()).toEqual(['@vendor/package-b', 'package-a'])
-    } finally {
-      await Promise.all([
-        execa.command('yarn unlink', { cwd: P.resolve(userHome, 'package-a') }),
-        execa.command('yarn unlink', { cwd: P.resolve(userHome, 'package-b') }),
-      ])
-      await Promise.all([
-        remove(P.resolve(userHome, 'package-b')),
-        remove(P.resolve(userHome, 'package-a')),
-      ])
-    }
-  },
+  'two links': () =>
+    withLocaltmpDir(async () => {
+      await outputFiles({
+        'package-a/package.json': JSON.stringify({ name: 'package-a' }),
+        'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
+      })
+      await execa.command('yarn link', { cwd: 'package-a' })
+      await execa.command('yarn link', { cwd: 'package-b' })
+      try {
+        expect(await self()).toEqual(['@vendor/package-b', 'package-a'])
+      } finally {
+        await Promise.all([
+          execa.command('yarn unlink', { cwd: 'package-a' }),
+          execa.command('yarn unlink', { cwd: 'package-b' }),
+        ])
+        await Promise.all([remove('package-b'), remove('package-a')])
+      }
+    }),
 }
