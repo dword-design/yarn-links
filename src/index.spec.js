4ea2d2fcf140f6c8e14f1739598b60338b560aed
dword <info@dword-design.de>
2021-03-31 20:51:04 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,9 @@
 import execa from 'execa'
-import globby from 'globby'
 import outputFiles from 'output-files'
 import P from 'path'
 import withLocaltmpDir from 'with-local-tmp-dir'
 import processOnSpawn from 'process-on-spawn'
-
+import { remove } from 'fs-extra'
 import self from '.'
 import yarnLinksPath from './yarn-links-path'
 
@@ -23,18 +22,14 @@ export default {
         execa.command('yarn link', { cwd: 'package-b' }),
       ])
       processOnSpawn.removeListener(removeSpawnWrap)
-      /*await Promise.all([
-        execa.command('yarn link', { cwd: 'package-a' }),
-        execa.command('yarn link', { cwd: 'package-b' }),
-      ])*/
       try {
         expect(await self()).toEqual(['@vendor/package-b', 'package-a'])
       } finally {
         // yarn unlink currently does not work with GitHub Actions
-        /*await Promise.all([
+        await Promise.all([
           remove(P.join(yarnLinksPath, '@vendor')),
           remove(P.join(yarnLinksPath, 'package-a')),
-        ])*/
+        ])
       }
     }),
 }
