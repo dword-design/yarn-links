d5e013d733c5555e4aaa0b3f4ff678c4ba1f2a02
dword <info@dword-design.de>
2021-03-31 20:47:43 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,6 +3,7 @@ import globby from 'globby'
 import outputFiles from 'output-files'
 import P from 'path'
 import withLocaltmpDir from 'with-local-tmp-dir'
+import processOnSpawn from 'process-on-spawn'
 
 import self from '.'
 import yarnLinksPath from './yarn-links-path'
@@ -15,10 +16,17 @@ export default {
         'package-a/package.json': JSON.stringify({ name: 'package-a' }),
         'package-b/package.json': JSON.stringify({ name: '@vendor/package-b' }),
       })
+      const removeSpawnWrap = opts => (opts.env.NODE_OPTIONS = '')
+      processOnSpawn.addListener(removeSpawnWrap)
       await Promise.all([
         execa.command('yarn link', { cwd: 'package-a' }),
         execa.command('yarn link', { cwd: 'package-b' }),
       ])
+      processOnSpawn.removeListener(removeSpawnWrap)
+      /*await Promise.all([
+        execa.command('yarn link', { cwd: 'package-a' }),
+        execa.command('yarn link', { cwd: 'package-b' }),
+      ])*/
       try {
         expect(await self()).toEqual(['@vendor/package-b', 'package-a'])
       } finally {
